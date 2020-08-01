a8c40c1362fc2b9f39d0f202faede04f3a86f173
Nycollas <nycollas.duaarte@hotmail.com>
2020-08-01 02:44:47 -0300

    added days in DayScreen

--- src/screens/mainScreen/days/index.tsx
+++ src/screens/mainScreen/days/index.tsx
@@ -4,32 +4,47 @@ import { View, Text } from 'react-native';
 import styles from './styles';
 
 export default function Days() {
-  const todayDay = new Date().getDate();
-  const todayWeek = new Date().getDay();
   const weekList = ['Dom', 'Seg', 'Terç', 'Qua', 'Qui', 'Sex', 'Sáb', 'Dom'];
   
+  function calculatePreviouDays(num: number) {
+    let numberDaysMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    let day = new Date().getDate();
+    let month = new Date().getMonth();
 
+    if(day - num <= 0) {
+      day += numberDaysMonth[month-1];
+      day -=num;
+    } else {
+      day -= num;
+    }
+    
+    return day;
+  }
+
+  function calculatePreviouWeeks(num: number) {
+
+  }
 
   return(
     <View style={styles.container}>
       <View style={styles.row}>
-        <Text style={styles.todayDate}>{weekList[0]}</Text>
-        <Text style={styles.todayDate}>{weekList[1]}</Text>
-        <Text style={styles.todayDate}>{weekList[2]}</Text>
-        <Text style={styles.todayDate}>{weekList[3]}</Text>
-        <Text style={styles.todayDate}>{weekList[4]}</Text>
-        <Text style={styles.todayDate}>{weekList[5]}</Text>
-        <Text style={[styles.todayDate, styles.today]}>{weekList[6]}</Text>
+        <Text style={styles.todayDate}>{calculatePreviouDays(6)}</Text>
+        <Text style={styles.todayDate}>{calculatePreviouDays(5)}</Text>
+        <Text style={styles.todayDate}>{calculatePreviouDays(4)}</Text>
+        <Text style={styles.todayDate}>{calculatePreviouDays(3)}</Text>
+        <Text style={styles.todayDate}>{calculatePreviouDays(2)}</Text>
+        <Text style={styles.todayDate}>{calculatePreviouDays(1)}</Text>
+        <Text style={styles.todayDate}>{calculatePreviouDays(0)}</Text>
       </View>
 
       <View style={styles.row}>
-        <Text style={styles.todayDate}>{todayDay-6}</Text>
-        <Text style={styles.todayDate}>{todayDay-5}</Text>
-        <Text style={styles.todayDate}>{todayDay-4}</Text>
-        <Text style={styles.todayDate}>{todayDay-3}</Text>
-        <Text style={styles.todayDate}>{todayDay-2}</Text>
-        <Text style={styles.todayDate}>{todayDay-1}</Text>
-        <Text style={[styles.todayDate, styles.today]}>{todayDay}</Text>
+        <Text style={styles.todayDate}>{}</Text>
+        <Text style={styles.todayDate}>{}</Text>
+        <Text style={styles.todayDate}>{}</Text>
+        <Text style={styles.todayDate}>{}</Text>
+        <Text style={styles.todayDate}>{}</Text>
+        <Text style={styles.todayDate}>{}</Text>
+        <Text style={[styles.todayDate, styles.today]}>{}</Text>
       </View>
     </View>
   );
